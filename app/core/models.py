050907cc9b3c9655e43f0d42ec795d119ace4fa6
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2021-01-16 14:16:47 +0100

    added endpoint for creating users

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser,
-                                        BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                       PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -28,9 +29,9 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model with email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
